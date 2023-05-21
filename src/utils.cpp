dd31670d183fdf71ba0cbb9d4e8a98f0a2fdf5c0
Pralad Prasad <pralad.prasad@gmail.com>
2023-05-21 15:20:34 +0200

    Added helper functions for time handling

---
+++ src/utils.cpp
@@ -0,0 +1,19 @@
+//
+// Created by prlpr on 20/05/2023.
+//
+
+#include "utils.h"
+
+std::string getCurrentTime() {
+    std::chrono::system_clock::time_point p = std::chrono::system_clock::now();
+    time_t t = std::chrono::system_clock::to_time_t(p);
+    char str[26];
+    ctime_s(str, sizeof str, &t);
+    return str;
+}
+
+std::wstring convertToWideString(const std::string &str) {
+    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+    std::wstring wide_str = converter.from_bytes(str);
+    return wide_str;
+}
