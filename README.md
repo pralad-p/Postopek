9cf183d9ddb50b77e221897af6586920120f3ba6
Pralad Prasad <pralad.prasad@gmail.com>
2023-05-21 18:38:53 +0200

    Interactivity handled

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Procedure CLI application for Windows-based checklists.
   - Include files in 'inc', source files in 'src', and libraries in 'libs', `main.cpp` is the entry point.
 - [x] Incorporate FTXUI into project
 - [x] Make big clock TUI
-- [ ] Make clock and static list work together
-  - Somewhat works, but list interactivity doesn't
+- [x] Make clock and static list work together
+  - Interactivity is good; but making it all work together (what a challenge)
--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,80 @@
-#include <ftxui/component/screen_interactive.hpp>
-#include <ftxui/component/component.hpp>
-#include <ftxui/dom/elements.hpp>
-#include <chrono>
-#include <thread>
+#include <memory>   // for shared_ptr, allocator, __shared_ptr_access
+#include <thread>   // for thread, sleep_for
+#include <chrono>   // for milliseconds
 #include <array>
-#include "utils.h"
-#include "ftxui/component/loop.hpp"
 
-int main() {
-    using namespace ftxui;
-    // Initialize the screen.
-    auto screen = ScreenInteractive::TerminalOutput();
+#include "ftxui/component/component.hpp"              // for Renderer, Checkbox, Container, Vertical
+#include "ftxui/component/component_base.hpp"         // for ComponentBase
+#include "ftxui/component/screen_interactive.hpp"     // for ScreenInteractive
+#include "ftxui/dom/elements.hpp"                     // for operator|, Element, text, bold, border, center, color
+#include "ftxui/screen/color.hpp"                     // for Color, Color::Red
+
+using namespace ftxui;
+
+int main(int argc, const char *argv[]) {
+    auto screen = ScreenInteractive::FitComponent();
+    auto button_quit = Button("Quit", screen.ExitLoopClosure());
+    screen.Clear();
+
     // Create a vector of strings for the checklist items.
     std::vector<std::string> items = {"Marinate chicken", "Cut them up", "Cook"};
 
-    // Create a vector of booleans to track which items are checked.
-    std::array<bool, 3> checked = {false, false, true};
+    // Create a vector of booleans to track which items are states.
+    std::array<bool, 3> states = {false, false, false};
+    auto checkboxContainer = Container::Vertical({});
 
     // Create a vector of Checkbox components for the checklist.
     std::vector<Component> checkboxes;
     for (size_t i = 0; i < items.size(); ++i) {
-        CheckboxOption option;
-        option.transform = [](const EntryState &state) {
-            return hbox({
-                                text(state.state ? "[x] " : "[ ] "),
-                                text(state.label) | bold,
-                        }) | (state.focused ? inverted : nothing);
-        };
-        checkboxes.push_back(Checkbox(&items[i], &checked[i], option));
+        checkboxContainer->Add(Checkbox(&items[i], &states[i]));
     }
 
-    // Create a Renderer component to display the clock and the checklist.
-    auto renderer = Renderer([&] {
-        // Create a string to represent the current time.
-        std::string time_str = getCurrentTime();
+    // Create the text element for the counter.
+    int counter = 0;
 
-        // Create a vector of elements for the checklist.
-        std::vector<Element> checklist;
-        for (size_t i = 0; i < items.size(); ++i) {
-            checklist.push_back(checkboxes[i]->Render() | flex);
-        }
-
-        // Return the DOM for the application.
+    // Create a Renderer component for the counter text element.
+    auto counterRenderer = Renderer([&] {
         return vbox({
-                            text(time_str) | color(Color::CornflowerBlue) | bold | center | border,
-                            separator(),
-                            text("Procedure: Make chicken") | underlined | center,
-                            separator(),
-                            vbox(checklist),
+                            text("Counter: ") | center | bold | border,
+                            text(std::to_string(counter))
                     });
     });
 
-    // Run the application in a loop.
-    std::thread([&] {
+    // Separate thread for updating the checkbox state.
+//    std::thread updateThread([&] {
+//        while (true) {
+//            // Perform some logic here to update the checkbox state.
+//            checkboxState = !checkboxState;
+//
+//            // Post an event to the main thread to trigger a re-render.
+//            screen.PostEvent(Event::Custom);
+//
+//            // Sleep for 1 second.
+//            std::this_thread::sleep_for(std::chrono::seconds(5));
+//        }
+//    });
+
+    std::thread counterThread([&] {
         while (true) {
-            std::this_thread::sleep_for(std::chrono::seconds(1));
+            // Perform some logic here to update the counter.
+            counter++;
+
+            // Post an event to the main thread to trigger a re-render.
             screen.PostEvent(Event::Custom);
+
+            // Sleep for 2 seconds.
+            std::this_thread::sleep_for(std::chrono::seconds(2));
         }
-    }).detach();
+    });
 
-    screen.Loop(renderer);
+    // Run the render loop for the text element in the main thread.
+    screen.Loop(Container::Vertical({
+                                            counterRenderer,
+                                            checkboxContainer,
+                                            button_quit
+                                    }));
 
+    // Join the update thread to properly exit the application.
+    counterThread.join();
     return 0;
 }
