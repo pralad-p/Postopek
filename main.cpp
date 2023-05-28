2ad5b4f67c3a15586030f6289e63d60e77e9bc5f
Pralad Prasad <pralad.prasad@gmail.com>
2023-05-28 12:49:05 +0200

    closes #21

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@
  * System libraries
  */
 #include <sstream>
+#include <filesystem>
+
 
 /*
  * Own module libraries
@@ -17,8 +19,81 @@
 #include "utils.h"
 #include "Application.h"
 #include "components/personalComponents.h"
+#include "validation.h"
+
+
+// trimStringInPlace from start
+static inline void ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
+}
+
+// trimStringInPlace from end
+static inline void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+}
+
+// trimStringInPlace from both ends
+static inline void trimStringInPlace(std::string &s) {
+    ltrim(s);
+    rtrim(s);
+}
+
+std::vector<std::filesystem::path> checkTempFileAndGetFiles() {
+    // Get the path to the TEMP folder
+    auto tempFolderPath = std::filesystem::temp_directory_path();
+    std::string config_file = "postopek_files.config";
+
+    // Construct the full path to the file in the TEMP folder
+    std::filesystem::path configFilePath = tempFolderPath / config_file;
+
+    // Check if the file exists and it is a regular file
+    if (!std::filesystem::is_regular_file(configFilePath)) {
+        throw std::runtime_error("Not a valid file! Run first_run.bat first!");
+    }
+
+    std::ifstream file(configFilePath);
+    if (!file.is_open()) {
+        throw std::runtime_error("Failed to open file: " + configFilePath.string());
+    }
+    std::string line;
+    while (std::getline(file, line)) {
+        // Find the position of the '=' character
+        size_t delimiterPos = line.find('=');
+        if (delimiterPos != std::string::npos) {
+            // Extract the key and value from the line
+            std::string lineKey = line.substr(0, delimiterPos);
+            std::string lineValue = line.substr(delimiterPos + 1);
+            trimStringInPlace(lineKey);
+            trimStringInPlace(lineValue);
+            // Check if the extracted key matches the desired key
+            if (lineKey == "MARKDOWN_FILES_DIR") {
+                std::filesystem::path mdDirectoryPath = std::filesystem::path(lineValue);
+                std::vector<std::filesystem::path> markdownFilePaths;
+
+                for (const auto &entry: std::filesystem::directory_iterator(mdDirectoryPath)) {
+                    if (entry.is_regular_file() && entry.path().extension() == ".md") {
+                        markdownFilePaths.push_back(entry.path());
+                    }
+                }
+                if (markdownFilePaths.empty()) {
+                    throw std::runtime_error("No markdown files in directory!");
+                }
+                return markdownFilePaths;
+            }
+        }
+    }
+
+
+}
+
 
 int main() {
+    // Read from config file
+    auto mdPaths = checkTempFileAndGetFiles();
     // Variables
     bool checked = false;
     auto checkboxLabel = [&]() -> std::wstring {
@@ -97,23 +172,23 @@ int main() {
 
     auto filler_component = ftxui::Renderer([] { return ftxui::filler(); });
 
-    auto container = ftxui::Container::Vertical({
-                                                        timeRenderer,
-                                                        hoverable_checkbox,
-                                                        hover_text_renderer,
-                                                        filler_component,
-                                                        ftxui::Container::Horizontal({
-                                                                                             input_component |
-                                                                                             ftxui::borderRounded,
-                                                                                             updateButton()
-                                                                                     }),
-                                                        ftxui::Renderer([] {
+    auto taskContainer = ftxui::Container::Vertical({
+                                                            timeRenderer,
+                                                            hoverable_checkbox,
+                                                            hover_text_renderer,
+                                                            filler_component,
+                                                            ftxui::Container::Horizontal({
+                                                                                                 input_component |
+                                                                                                 ftxui::borderRounded,
+                                                                                                 updateButton()
+                                                                                         }),
+                                                            ftxui::Renderer([] {
                                                             return ftxui::text("qqq ▶️ Quit");
                                                         })
                                                 });
 
     // Replace the main component with the engine wrapper
-    auto main_component = ftxui::Make<Application>(container, screen.ExitLoopClosure());
+    auto main_component = ftxui::Make<Application>(taskContainer, screen.ExitLoopClosure());
 
     // Run the application in a loop.
     std::thread([&] {
