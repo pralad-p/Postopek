a61dc2d3df2917c15ce335cba8678848dda71f11
Pralad Prasad <pralad.prasad@gmail.com>
2023-05-28 12:56:45 +0200

    better organization

--- src/utils.cpp
+++ src/utils.cpp
@@ -2,7 +2,6 @@
 // Created by prlpr on 20/05/2023.
 //
 
-#include <string>
 #include "utils.h"
 
 #pragma clang diagnostic push
@@ -35,6 +34,73 @@ std::string convertToHoursMinutes(const std::string &timeString) {
     // Pad the hour with a leading zero if necessary
     std::string hourString = (hour < 10) ? "0" + std::to_string(hour) : std::to_string(hour);
     return hourString + hhmm.substr(2) + " " + amPm;
+}
+
+// trimStringInPlace from start
+static inline void ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !isspace(ch);
+    }));
+}
+
+// trimStringInPlace from end
+static inline void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !isspace(ch);
+    }).base(), s.end());
+}
+
+// trimStringInPlace from both ends
+static inline void trimStringInPlace(std::string &s) {
+    ltrim(s);
+    rtrim(s);
+}
+
+std::vector<std::filesystem::path> checkTempFileAndGetFiles() {
+    // Get the path to the TEMP folder
+    auto tempFolderPath = std::filesystem::temp_directory_path();
+    std::string config_file = "postopek_files.config";
+
+    // Construct the full path to the file in the TEMP folder
+    std::filesystem::path configFilePath = tempFolderPath / config_file;
+
+    // Check if the file exists and it is a regular file
+    if (!std::filesystem::is_regular_file(configFilePath)) {
+        throw std::runtime_error("Not a valid file! Run first_run.bat first!");
+    }
+
+    std::ifstream file(configFilePath);
+    if (!file.is_open()) {
+        throw std::runtime_error("Failed to open file: " + configFilePath.string());
+    }
+    std::string line;
+    while (std::getline(file, line)) {
+        // Find the position of the '=' character
+        size_t delimiterPos = line.find('=');
+        if (delimiterPos != std::string::npos) {
+            // Extract the key and value from the line
+            std::string lineKey = line.substr(0, delimiterPos);
+            std::string lineValue = line.substr(delimiterPos + 1);
+            trimStringInPlace(lineKey);
+            trimStringInPlace(lineValue);
+            // Check if the extracted key matches the desired key
+            if (lineKey == "MARKDOWN_FILES_DIR") {
+                std::filesystem::path mdDirectoryPath = std::filesystem::path(lineValue);
+                std::vector<std::filesystem::path> markdownFilePaths;
+
+                for (const auto &entry: std::filesystem::directory_iterator(mdDirectoryPath)) {
+                    if (entry.is_regular_file() && entry.path().extension() == ".md") {
+                        markdownFilePaths.push_back(entry.path());
+                    }
+                }
+                if (markdownFilePaths.empty()) {
+                    throw std::runtime_error("No markdown files in directory!");
+                }
+                return markdownFilePaths;
+            }
+        }
+    }
+    return std::vector<std::filesystem::path>{};
 }
 
 #pragma clang diagnostic pop
--- src/utils.h
+++ src/utils.h
@@ -8,11 +8,22 @@
 #include <chrono>
 #include <ctime>
 #include <codecvt>
+#include <filesystem>
+#include <string>
+#include "validation.h"
 
 std::string getCurrentTime();
 
 std::wstring convertToWideString(const std::string &);
 
 std::string convertToHoursMinutes(const std::string &);
+
+static inline void ltrim(std::string &);
+
+static inline void rtrim(std::string &);
+
+static inline void trimStringInPlace(std::string &);
+
+std::vector<std::filesystem::path> checkTempFileAndGetFiles();
 
 #endif //POSTOPEK_UTILS_H
